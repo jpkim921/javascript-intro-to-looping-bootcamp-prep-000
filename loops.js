3fb4f76e04b36f2a7fd202b31d5896476b846840
jpkim921 <jpkim921@gmail.com>
2017-11-25 19:11:10 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,8 @@
+for (var i = 0; i < 25; i++ ){
+  var array = [];
+  if(i === 1){
+    array.push("I am 1 strange loop.")
+  } else {array.push(`I am ${i} strange loops.`)    
+  }
+  return array;
+}
